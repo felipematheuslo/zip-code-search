3a48e7f9fe06e9b677b25b1304c293def59f030b
Felipe Laurindo <felipe_nu@hotmail.com>
2023-02-03 20:04:17 -0400

    Create

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import { FaSearch } from 'react-icons/fa';
+import { useState } from 'react';
+import api from './services/api';
+import './styles.css';
+
+function App() {
+  const [input, setInput] = useState('')
+  const [cep, setCep] = useState({})
+
+  async function handleSearch(){
+    if(input === ''){
+      alert("Digite o cep!")
+      return;
+    }
+
+    try{
+      const response = await api.get(`${input}/json`);
+      setCep(response.data)
+      setInput('')
+    }catch{
+      alert("Algo deu errado!")
+      setInput('')
+    }
+  }
+
+  return (
+    <div className="conteiner">
+      <h1 className="title">BUSCADOR CEP</h1>
+
+      <div className="conteinerInput">
+        <input
+        type="text"
+        placeholder="Digite seu CEP"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        />
+
+        <button className="buttonSearch" onClick={handleSearch}>
+          <FaSearch size={20} color='white'/>
+        </button>
+      </div>
+
+      {Object.keys(cep).length > 0 && (
+        <main className='main'>
+          <h2>CEP: {cep.cep}</h2>
+
+          <span>{cep.logradouro}</span>
+          <span>{cep.bairro}</span>
+          <span>{cep.localidade} - {cep.uf}</span>
+        </main>
+      )}
+
+    </div>
+  );
+}
+
+export default App;
